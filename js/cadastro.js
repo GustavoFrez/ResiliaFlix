d3ede0928513180f761c0ff53bf5bc9bc89b44a6
Pedro <mafuzao@outlook.com>
2021-05-18 08:07:42 -0300

    ViaCep implementada

--- js/cadastro.js
+++ js/cadastro.js
@@ -1,4 +1,66 @@
-let botaoEnviar = jQuery('#botaoCadastro').click(function(){
+$(document).ready(function(){
+    $('#validationCustom05').focusout(function(){
+        let cep = $('#validationCustom05').val();
+        cep = cep.replace('-', '');
+
+        let urlId = 'https://viacep.com.br/ws/'+ cep +'/json/';
+
+        $.ajax({
+            url: urlId,
+            type: 'get',
+            dataType: 'json',
+            success: function(data){
+                console.log(data)
+                $('#rua').val(data.logradouro);
+                $('#bairro').val(data.bairro);
+                $('#cidade').val(data.localidade);
+                $('#estado').val(data.uf);
+            },
+            error: function(erro){
+                console.log(erro)
+            }
+        })
+    })
+})
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+/*let botaoEnviar = jQuery('#botaoCadastro').click(function(){
     console.log('botao funfa')
     validaCep()
 })
@@ -18,4 +80,4 @@ async function validaCep(){
     console.log(request)
 }
 const cep = $('#validationCustom05').value
-console.log(cep)
+console.log(cep)*/
