6eb97d3d421500179664eece2b10120af758a9e8
GustavoFrez <gustavo_frez@hotmail.com>
2021-05-19 14:13:02 -0300

    Finalização página login

---
+++ js/login.js
@@ -0,0 +1,14 @@
+
+//$('#rform').submit(false)
+
+$('#envia').on('click', function () {
+    var email = $('#email').val()
+    var senha = $('#senha').val()
+
+    //console.log(email)
+    //console.log(senha)
+})
+
+
+
+
